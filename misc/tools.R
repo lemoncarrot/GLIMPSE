32a1cd21f34b08d0208959ab884f0b24cd595496
lemoncarrot <78185366+lemoncarrot@users.noreply.github.com>
2024-03-13 00:22:32 -0500

    Update tools.R
    updating tools

--- misc/tools.R
+++ misc/tools.R
@@ -12,3 +12,32 @@ ensembl_ids <- mapping_vector[single_gene_list]
 #ensembl_ids <- na.omit(ensembl_ids)
 common_ids <- intersect(ensembl_ids, bulk_gene_list)
 #write.csv(common_ids, "common_genes.csv", row.names=FALSE)
+
+library(googleCloudStorageR)
+
+#Sys.setenv(GCS_AUTH_FILE = "/home/dbr_journalclub/amazing-craft-416704-a9265357ed48.json")
+#library(googleCloudStorageR)
+#gcs_auth()
+
+#bucket_name <- "writebucket"
+#object_name <- "meth_1.png"
+#file_path <- "/home/dbr_journalclub/meth_1.png"
+
+#gcs_upload(file_path, bucket = bucket_name, name = object_name)
+
+#convert cpg to genes
+library(FDb.InfiniumMethylation.hg19)
+filter01 <- read.csv("DMR_0_1.csv")
+filter02 <- read.csv("DMR_0_2.csv")
+sig.cpg <- filter02$SiteNames
+hm450 <- get450k()
+probes <- hm450[sig.cpg]
+genes <- getNearestTSS(probes)
+df <- as.data.frame(genes)
+write.csv(df, "DMR_genes_1.csv")
+
+#convert df column to pasteable format (for shinygo)
+values <- df$nearestGeneSymbol
+
+values_collapsed <- paste(values, collapse = ",")
+writeLines(values_collapsed, "paste2.txt")
